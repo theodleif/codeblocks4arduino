22da225216c4931716d3397de8ca133beb6224bd
theodleif <theodleif@googlemail.com>
2016-02-07 15:07:02 +0100

    changed to UTF8

--- hack.cpp
+++ hack.cpp
@@ -1,10 +1,12 @@
 bool FileManager::Save(const wxString& name, const wxString& data, wxFontEncoding encoding, bool bom)
 {
         wxFile f;
+    	wxCSConv conv(encoding);
+	wxCharBuffer buf = data.mb_str(conv);
 	size_t len;
-	len = data.Len();
+	len = strlen(buf);
 	if (!f.Open(name, wxFile::write, wxS_DEFAULT)) {return false;}
-	if (f.Write(data, len) != len) {return false;}
+	if (f.Write(buf, len) != len) {return false;}
 	if (!f.Close()) {return false;}
 	return true;
 }
